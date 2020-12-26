ea0b7fffa78a9d92754b8446b2ad06af60d1a2ee
DSaurus <>
2020-12-27 01:27:52 +0800

    config and load/save

--- plan.py
+++ plan.py
@@ -1,27 +1,62 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-pts = np.zeros((15, 2), dtype=float)
 
-f = open('pts.txt', 'r')
-for i in range(15):
-    line = f.readline().split(' ')
-    pts[i][0] = float(line[0])
-    pts[i][1] = float(line[1])
-pts = torch.FloatTensor(pts)
-plan = []
-for i in range(15):
-    plan.append(int(f.readline()))
+def config_parser():
+    import configargparse
+    parser = configargparse.ArgumentParser()
+    parser.add_argument("--init", action='store_true')
+    parser.add_argument("--workdir", type=str, default='test')
+    parser.add_argument("--checkpoint", type=str, default='test')
+    return parser
 
-# load(pts_plan)
-pts_plan = []
-for i in range(7*14):
-    pts_plan.append(torch.tensor([0.0, 0.0], requires_grad=True))
-# pts_plan = torch.cat(pts_plan, dim=0)
+def load_data(filename):
+    pts = np.zeros((15, 2), dtype=float)
+    f = open(filename, 'r')
+    for i in range(15):
+        line = f.readline().split(' ')
+        pts[i][0] = float(line[0])
+        pts[i][1] = float(line[1])
+    pts = torch.FloatTensor(pts)
+    plan = []
+    for i in range(15):
+        plan.append(int(f.readline()))
+    return pts, plan
+
+def load_checkpoint(filename):
+    f = open(filename, 'r')
+    pts_plan = []
+    for i in range(7*14):
+        x, y = f.readline().split(' ')
+        pts_plan.append(torch.tensor([float(x), float(y)], requires_grad=True))
+    return pts_plan
+
+def save_checkpoint(workdir, pts_plan):
+    filename = os.path.join(workdir, 'checkpoint.txt')
+    f = open(filename, 'w')
+    for i in range(7*14):
+        f.write("%.18f %.18f\n" % (pts_plan[i][0], pts_plan[i][1]))
+
+
+parser = config_parser()
+config = parser.parse_args()
+workdir = config.workdir
+os.makedirs(workdir, exist_ok=True)
+
+pts, plan = load_data('pts.txt')
+
+if config.init:
+    pts_plan = []
+    for i in range(7*14):
+        pts_plan.append(torch.tensor([0.0, 0.0], requires_grad=True))
+else:
+    pts_plan = load_checkpoint(config.checkpoint)
+
 optim = torch.optim.Adam(pts_plan, lr=1e-1)
 
-for _ in range(1000):
+for _ in range(1):
     loss = 0
     for i in range(14):
         loss += torch.sum((pts_plan[i*7] - pts[plan[i]])**2)
@@ -36,7 +71,7 @@ for _ in range(1000):
     optim.step()
     print(_)
 
-# save(pts_plan)
+save_checkpoint(workdir, pts_plan)
 x = []
 y = []
 for i in range(7*14):
@@ -44,7 +79,7 @@ for i in range(7*14):
     y.append(pts_plan[i][1])
 plt.plot(x, y, '-')
 plt.plot(x, y, '.')
-plt.savefig('test.jpg')
+plt.savefig(os.path.join(workdir, 'test.jpg'))
 
 
 
---
+++ run.sh
@@ -0,0 +1,1 @@
+python plan.py --checkpoint test/checkpoint.txt
